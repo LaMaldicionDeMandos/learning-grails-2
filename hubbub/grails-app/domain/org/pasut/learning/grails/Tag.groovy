55c263149162867a5b2931162e26705740f08193
MatiasGinart <matiasginart@gmail.com>
2016-02-04 17:45:08 -0300

    Agrego atributos a tag y la relacion de muchos a muchos

--- hubbub/grails-app/domain/org/pasut/learning/grails/Tag.groovy
+++ hubbub/grails-app/domain/org/pasut/learning/grails/Tag.groovy
@@ -1,7 +1,14 @@
 package org.pasut.learning.grails
 
 class Tag {
-
+	String name
+	User user
+    
     static constraints = {
+    	name(black: false)
     }
+
+    static hasMany = [posts: Post]
+
+    static belongsTo = [User, Post] //La tag depende de los usuarios y los Post
 }
