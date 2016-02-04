7cc740c9d097989b561e6f0e67305cd99e4ab734
MatiasGinart <matiasginart@gmail.com>
2016-02-04 15:57:12 -0300

    agrego atributos y contraints a Profile

--- hubbub/grails-app/domain/org/pasut/learning/grails/Profile.groovy
+++ hubbub/grails-app/domain/org/pasut/learning/grails/Profile.groovy
@@ -4,6 +4,23 @@ class Profile {
 
 	static belongsTo = User	//Cascada, si se borra un User, se borra el Profile
 
-    static constraints = {
-    }
+	byte[] photo
+	String fullName
+	String bio
+	String homepage
+	String email
+	String timezone
+	String country
+	String jabberAddress
+
+	static {
+		fullName(nullable: true)
+		bio(nullable: true, maxSize: 1000)
+		homepage(nullable: true, url: true)
+		meail(nullable: true, email: true)
+		photo(nullable: true)
+		country(nullable: true)
+		timezone(nullable: true)
+		jabberAddress(nullable: true, email: true)
+	}
 }
