b38ca7802803072a0ff3c5f19611840d2b024da8
MatiasGinart <matiasginart@gmail.com>
2016-02-04 15:59:08 -0300

    agrego profile al suario y agrego las constraints

--- hubbub/grails-app/domain/org/pasut/learning/grails/User.groovy
+++ hubbub/grails-app/domain/org/pasut/learning/grails/User.groovy
@@ -5,10 +5,13 @@ class User {
 	String password
 	String homepage
 	Date dateCreated
+	Profile profile
 
 	static constraints = {
 		userId(size: 3..20, unique: true)
 		password(size: 6..8, validator: {pass, user -> pass != user.userId})
 		homepage(url: true, nullable: true)
+		dateCreated()
+		profile(nullable: true)
 	}
 }
