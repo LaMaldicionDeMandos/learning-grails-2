2bc56df7ba7e442903c8afcaed14b98ed0f77652
MatiasGinart <matiasginart@gmail.com>
2016-02-04 16:16:21 -0300

    agrego un test de integracion para demostrar que por la relacion one-to-many se generan los metodos addToPosts y removeFromPosts

---
+++ hubbub/test/integration/org/pasut/learning/grails/PostIntegrationIntegrationSpec.groovy
@@ -0,0 +1,15 @@
+package org.pasut.learning.grails
+
+import grails.test.spock.IntegrationSpec
+
+class PostIntegrationIntegrationSpec extends IntegrationSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
