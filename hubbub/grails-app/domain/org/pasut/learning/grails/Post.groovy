10a2f419e1dc269c051a36419e965cf4bd17f27e
MatiasGinart <matiasginart@gmail.com>
2016-02-04 16:09:19 -0300

    agrego atributos a Post y agrego una referencia al User (owner)

--- hubbub/grails-app/domain/org/pasut/learning/grails/Post.groovy
+++ hubbub/grails-app/domain/org/pasut/learning/grails/Post.groovy
@@ -1,7 +1,12 @@
 package org.pasut.learning.grails
 
 class Post {
+	String content
+	Date dateCreated
+
+	static belongsTo = [user: User] // Tengo una referencia al padre
 
     static constraints = {
+    	content(black: false)
     }
 }
