cdd007e7d1ac28b01988377b816c242e9d58854a
MatiasGinart <matiasginart@gmail.com>
2016-02-10 15:53:11 -0300

    Agrego la accion timeline, en la URL de grails por defecto se usa /app/controller/action/id, por lo tanto, tengo el params.id disponible por default. Por ejemplo /hubbub/post/timeline/aida, params.id = aida

--- hubbub/grails-app/controllers/org/pasut/learning/grails/PostController.groovy
+++ hubbub/grails-app/controllers/org/pasut/learning/grails/PostController.groovy
@@ -2,4 +2,9 @@ package org.pasut.learning.grails
 
 class PostController {
     def scaffold = true
+
+    def timeline = {
+        def user = User.findByUserId(params.id)
+        [user: user]
+    }
 }
