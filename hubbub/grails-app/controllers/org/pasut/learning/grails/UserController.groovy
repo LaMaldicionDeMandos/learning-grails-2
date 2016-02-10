b13ff39923eec027be2b5a81ed55f7176cf76bfb
MatiasGinart <matiasginart@gmail.com>
2016-02-10 09:38:39 -0300

    Agrego la accion 'results' que busca el usuario con el parametro 'userId' que viene del cuadro de texto, el metodo 'findAllByUserIdLike' lo agrega grails como en los Repository de spring'

--- hubbub/grails-app/controllers/org/pasut/learning/grails/UserController.groovy
+++ hubbub/grails-app/controllers/org/pasut/learning/grails/UserController.groovy
@@ -4,6 +4,11 @@ class UserController {
     def scaffold = true
 
     def search = {
-        
+
+    }
+
+    def results = {
+        def users = User.findAllByUserIdLike(params.userId)
+        return [users: users, term: params.userId]
     }
 }
