9bde3fd902f4bf1aa223b5f95c1402c5f6dcd94a
MatiasGinart <matiasginart@gmail.com>
2016-02-10 15:47:29 -0300

    agrego configuracion al iniciar la app, en este caso, creo un usario admin, solo para el ambiente de desarrollo

--- hubbub/grails-app/conf/BootStrap.groovy
+++ hubbub/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,21 @@
+import grails.util.Environment
+import org.pasut.learning.grails.Profile
+import org.pasut.learning.grails.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Environment.current.equals(Environment.DEVELOPMENT)) {
+            populateAdminUser()
+        }
     }
     def destroy = {
+    }
+
+    void populateAdminUser() {
+        if(!User.findByUserId('admin')) {
+            def profile = new Profile(email: 'admin@hubbub.com')
+            new User(userId: 'admin', password: 'secret', profile: profile).save()
+        }
     }
 }
