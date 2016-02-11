4c0c8b80361fc3c59cc9912357cc6d273ad3664f
MatiasGinart <matiasginart@gmail.com>
2016-02-11 17:08:32 -0300

    implemento un filtro, en controller y action puedo poner willcards '*'

--- hubbub/grails-app/conf/org/pasut/learning/grails/LameSecurityFilters.groovy
+++ hubbub/grails-app/conf/org/pasut/learning/grails/LameSecurityFilters.groovy
@@ -3,15 +3,22 @@ package org.pasut.learning.grails
 class LameSecurityFilters {
 
     def filters = {
-        all(controller:'*', action:'*') {
+        //secureActions es un nombre que le pongo, puede ser cualquier cosa
+        secureActions(controller:'post', action:'(addPost|deletePost)') {
             before = {
-
+                if (params.logonId) {
+                    session.user = User.findByUserId(params.logonId)
+                }
+                if (!session.user) {
+                    redirect(controller: 'login', action: 'form')
+                    return false
+                }
             }
             after = { Map model ->
 
             }
             afterView = { Exception e ->
-
+                log.debug "Finished running ${controllerName} - ${actionName}"
             }
         }
     }
