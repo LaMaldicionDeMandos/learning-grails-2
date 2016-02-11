88e79adb8c5e49be7c491d33ba1f11bc086aff48
MatiasGinart <matiasginart@gmail.com>
2016-02-11 15:27:01 -0300

    implemento guardado de archivo en disco, esto no anda porque tengo que elegir el lugar donde cuardarlo, asi como crear la carpeta contendora

--- hubbub/grails-app/controllers/org/pasut/learning/grails/ImageController.groovy
+++ hubbub/grails-app/controllers/org/pasut/learning/grails/ImageController.groovy
@@ -6,10 +6,18 @@ class PhotoUploadCommand {
 }
 
 class ImageController {
+    def index = {
+        redirect(action: 'form')
+    }
     def upload = {PhotoUploadCommand command ->
         def user = User.findByUserId(command.userId)
         user.profile.photo = command.photo
         redirect(action: 'view', id: command.userId)
+    }
+
+    def rawUpload = {
+        def req = request.getFile('photo')
+        req.transferTo(new File('/hubbub/images/${params.userId}/photo.jpg'))
     }
     def form = {
         [userList: User.list()]
