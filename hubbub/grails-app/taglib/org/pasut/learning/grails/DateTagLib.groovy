0cf205ae988b3a57ac61444952072b4760ccf7db
MatiasGinart <matiasginart@gmail.com>
2016-02-12 17:16:49 -0300

    Creo una taglib usando el comando 'create-tag-lib Date'

---
+++ hubbub/grails-app/taglib/org/pasut/learning/grails/DateTagLib.groovy
@@ -0,0 +1,6 @@
+package org.pasut.learning.grails
+
+class DateTagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+}
