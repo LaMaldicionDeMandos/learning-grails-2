28b6ce5df56cbd62cf04039999035ba63251d9e5
MatiasGinart <matiasginart@gmail.com>
2016-02-12 18:01:19 -0300

    implemento el metodo que parsea la fecha

--- hubbub/grails-app/taglib/org/pasut/learning/grails/DateTagLib.groovy
+++ hubbub/grails-app/taglib/org/pasut/learning/grails/DateTagLib.groovy
@@ -1,11 +1,45 @@
 package org.pasut.learning.grails
 
 class DateTagLib {
+    static final long second = 1000
+    static final long minute = second*60
+    static final long hour = minute*60
+    static final long day = hour*24
+
     static final namespace = 'h'
     def dateFromNow = { attrs ->
         def date = attrs.date
-        def niceDate = "Ahora!!"
+        def niceDate = getNiceDate(date)
 
         out << niceDate
+    }
+
+    static String getNiceDate(Date date) {
+        def now = new Date()
+        def diff = Math.abs(now.time - date.time)
+
+        def niceTime = ""
+        long calc = 0;
+        calc = Math.floor(diff / day)
+        if (calc) {
+            niceTime+=calc+"day"+(calc>1?"s":"")
+            diff %= day
+        }
+        calc = Math.floor(diff / hour)
+        if (calc) {
+            niceTime+=calc+"hour"+(calc>1?"s":"")
+            diff %= hour
+        }
+        calc = Math.floor(diff / minute)
+        if (calc) {
+            niceTime+=calc+"minute"+(calc>1?"s":"")
+            diff %= minute
+        }
+        if (!niceTime) {
+            niceTime = "Right now"
+        }else{
+            niceTime += (date.time > now.time) ? "from now" : "ago"
+        }
+        return niceTime
     }
 }
